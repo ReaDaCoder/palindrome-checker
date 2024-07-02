9bef42fd3b74a8f8668eb3d98e9bd246d13fea3e
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-02 15:15:27 +0200

    fixed buggs

--- scrits.js
+++ scrits.js
@@ -1,3 +1,5 @@
+
+
 let input = document.querySelector("input");
 let form = document.querySelector("btn");
 let h2 = document.querySelector("h2");
@@ -5,14 +7,13 @@ let h2 = document.querySelector("h2");
 function checker(){
     event.preventDefault();
 
-let lowerCaseInput = input.toLowerCase();
- let inputLen = input.value.length
+//let lowerCaseInput = input.toLowerCase();
+ let inputLen = input.value.length;
  for (var i = 0; i < inputLen; i++){
     if (input[i] !== input[inputLen - 1 - i]){
-        let h2 = document.querySelector("h2");
-        h2.innerHTML = `${input.value} is a NOT Palindrome`
+        h2.innerText = `${input.value} is a NOT Palindrome`
     } else{
-        h2.innerHTML = `${input.value} is a Palindrome`
+        h2.innerText = `${input.value} is a Palindrome`
     }
 
  }
@@ -54,4 +55,4 @@ input.value = ''
 }
 
 
-form.addEventListener("click", checker); */
+form.addEventListener("click", checker);*/
