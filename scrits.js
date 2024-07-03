e051da65d1a41bbf7ec7e0fe11bbf4ecd9e9cc80
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-03 10:24:50 +0200

    added event paremeter where the function is called

--- scrits.js
+++ scrits.js
@@ -1,59 +1,19 @@
 
+let input = document.querySelector('#input');
+let form = document.querySelector('#input-form');
+let h2 = document.querySelector('h2');
 
-let input = document.querySelector("#input");
-let form = document.querySelector("#btn");
-let h2 = document.querySelector("h2");
+function checker(event) {
+  event.preventDefault();
 
-function checker(event){
-    event.preventDefault();
-
-//let lowerCaseInput = input.toLowerCase();
- let inputLen = input.value.length;
- for (var i = 0; i < inputLen; i++){
-    if (input[i] !== input[inputLen - 1 - i]){
-        h2.innerText = `${input.value} is a NOT Palindrome`
-    } else{
-        h2.innerText = `${input.value} is a Palindrome`
+  let inputLen = input.value.length;
+  for (var i = 0; i < inputLen; i++) {
+    if (input.value[i] !== input.value[inputLen - 1 - i]) {
+      h2.innerText = `${input.value} is a NOT Palindrome`;
+    } else {
+      h2.innerText = `${input.value} is a Palindrome`;
     }
- }
- input.value = ''
- console.log(event);
+  }
+  input.value = '';
 }
-form.addEventListener("click", checker);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*let input = document.querySelector("#input");
- let form = document.querySelector("#btn");
- let h2 = document.querySelector("h2");
-
-
- function checker(){
-event.preventDefault();
-  let inputLen = input.value.length
-  for (var i = 0; i < inputLen; i++){
-     if (input[i] !== input[inputLen - 1 - i]){
-       
-        h2.innerText = ${input.value} is a NOT Palindrome
-    } else{
-        h2.innerText = ${input.value} is a Palindrome
-    }
-
- }
-
-input.value = ''
-}
-
-
-form.addEventListener("click", checker);*/
+form.addEventListener('click', (event) => checker(event));
