69015f7aa7782b60edc5707b2a7cecc6735ecce6
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-30 16:12:46 +0200

    console.log

--- scrits.js
+++ scrits.js
@@ -3,12 +3,15 @@ let h2 = document.querySelector("h2");
 
 function checker(event){
   event.preventDefault();
+  console.log(input);
   for(let i = 0; i < input.length; i++){
+    console.log("Is not a palindrome checker");
     if(input.value === input.value.split("").reverse().join("")){
       console.log("check");
     } else{
     //console.log("Is not a palindrome checker");
     }
+
   }
 }
 let form = document.getElementById("input-form");
