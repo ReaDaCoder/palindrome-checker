950d0f9a10f23a556a0c79710ef770d8a4600687
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-31 16:03:25 +0200

    added template literals

--- scrits.js
+++ scrits.js
@@ -3,12 +3,16 @@ let h2 = document.querySelector("h2");
 
 function checker(event){
   event.preventDefault();
-  for(let i = 0; i < input.length; i++){
-    let input = document.getElementById("input");
-    let h2 = document.querySelector("h2");
-
-    console.log(input);
+  for(let i = 0; i < input.value.length; i++){
+      h2.innerHTML = `${input.value} is a Palindrome`;
   }
 }
 let form = document.getElementById("input-form");
 form.addEventListener('click', checker);
+
+
+/*if(i=input.value){
+      h2.innerHTML = `${input.value} is a Palindrome`;
+    } else{
+      h2.innerHTML = `${input.value} is NOT Palindrome`;
+    }*/
