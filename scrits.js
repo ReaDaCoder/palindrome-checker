997657d05575dd1452293491bca69ee123bbc7d5
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-31 14:15:26 +0200

    created planning for palindrome checker

--- scrits.js
+++ scrits.js
@@ -1,12 +1,10 @@
-let input = document.getElementById("input");
-let h2 = document.querySelector("h2");
 
 function checker(event){
   event.preventDefault();
-  console.dir(input);
   for(let i = 0; i < input.length; i++){
-
-
+    let input = document.getElementById("input");
+  let h2 = document.querySelector("h2");
+    console.log(input);
   }
 }
 let form = document.getElementById("input-form");
