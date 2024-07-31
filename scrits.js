5acd91111c05d90f68012062e070537ae0654d44
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-31 11:34:03 +0200

    undated psuedocode

--- scrits.js
+++ scrits.js
@@ -1,16 +1,11 @@
-let input = document.getElementById("input").value;
+let input = document.getElementById("input");
 let h2 = document.querySelector("h2");
 
 function checker(event){
   event.preventDefault();
-  console.log(input);
+  console.dir(input);
   for(let i = 0; i < input.length; i++){
-    console.log("Is not a palindrome checker");
-    if(input.value === input.value.split("").reverse().join("")){
-      console.log("check");
-    } else{
-    //console.log("Is not a palindrome checker");
-    }
+
 
   }
 }
